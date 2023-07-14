51dde87c4a1d43a9bb9299202c7bf955af81d78a
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-07-13 21:28:48 -0600

    react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./components/Home.jsx";
+import { Login } from "./components/Login.jsx";
+import { Register } from "./components/Register.jsx";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="bg-slate-300 h-screen text-white flex">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
